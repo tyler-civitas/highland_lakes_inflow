89e7c73d29a0d678993b6175c4f9e645b95c63ec
tylerc-atx <tylerc-atx@github.com>
2017-05-23 14:15:57 -0500

    packing up

---
+++ src/hl_scraping.py
@@ -0,0 +1,17 @@
+import argparse
+from decimal import Decimal
+import datetime
+from time import sleep
+import pandas as pd
+
+
+from BeautifulSoup import BeautifulSoup
+import numpy as np
+import requests
+
+
+def create_gauge_list:
+
+
+
+if __name__ == "__main__":
